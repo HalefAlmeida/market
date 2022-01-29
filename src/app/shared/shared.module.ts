cab7a17a7a74bc08b88ea26a63ecb5cfa2487cc4
halefalmeida <halef.dorigan@edu.unipar.br>
2022-01-29 14:00:50 -0300

    added toolbar added footer added shared module added material module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MaterialModule } from './material/material.module';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [ToolbarComponent, FooterComponent],
+  imports: [CommonModule, MaterialModule],
+  exports: [MaterialModule, ToolbarComponent, FooterComponent],
+})
+export class SharedModule {}
