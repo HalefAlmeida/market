1832f3a82cb2d87555b151c1cdfd218b4984c61d
halefalmeida <halef.dorigan@edu.unipar.br>
2022-02-07 21:02:22 -0300

    added product list view

--- src/app/features/products/products.component.ts
+++ src/app/features/products/products.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Category } from 'src/app/shared/models/category';
+import { ProductService } from 'src/app/shared/services/product.service';
 
 @Component({
   selector: 'app-products',
@@ -8,18 +7,11 @@ import { Category } from 'src/app/shared/models/category';
   styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
-  form!: FormGroup;
+  products$: any;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      id: [null],
-      name: [null, [Validators.required]],
-      status: [true, [Validators.required]],
-      photoURL: [null, [Validators.required]],
-    });
+    this.products$ = this.productService.readAll();
   }
-
-  onSubmit() {}
 }
