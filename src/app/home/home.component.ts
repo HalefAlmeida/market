0791c879fd3fb9f6cfae287ec1559254b07e282d
halefalmeida <halef.dorigan@edu.unipar.br>
2022-02-21 21:43:38 -0300

    fixed categories visualization at home component added clicable category on home component added cols, rows and componentURL in category interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable, map, shareReplay } from 'rxjs';
+import { Category } from '../shared/models/category';
 import { CategoryService } from '../shared/services/category.service';
 
 @Component({
@@ -16,14 +18,38 @@ export class HomeComponent implements OnInit {
       shareReplay()
     );
 
-  categories$: any;
+  categories: Category[] = [];
+
+  tiles: any[] = [];
 
   constructor(
     private breakpointObserver: BreakpointObserver,
-    private categoryService: CategoryService
+    private categoryService: CategoryService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
-    this.categories$ = this.categoryService.readAll();
+    this.categoryService.readAll().subscribe((data) => {
+      data.map((item) => this.addTile(item));
+    });
+  }
+
+  private addTile(item: Category) {
+    this.tiles.push({
+      cols: item.cols,
+      rows: item.rows,
+      ...{
+        id: item.id,
+        name: item.name,
+        photoURL: item.photoURL,
+        status: item.status,
+        componentURL: item.componentURL,
+      },
+    });
+    console.log(this.tiles);
+  }
+
+  public navigateTo(url: string) {
+    this.router.navigateByUrl(url);
   }
 }
--- src/app/shared/models/category.ts
+++ src/app/shared/models/category.ts
@@ -3,4 +3,7 @@ export interface Category {
   name: string;
   status: boolean;
   photoURL: string;
+  cols: number; //number of collumns to be displayed in the home component
+  rows: number; //number of rows to be displayed in the home component
+  componentURL: string; //URL to navigate on click
 }
